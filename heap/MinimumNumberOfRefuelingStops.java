30e82125b4bc4b994b5ba38dbe70f49eda05c2d5
ashKIK <ashutheprogrammer@gmail.com>
2020-01-01 02:03:54 +0530

    update-heap

---
+++ heap/MinimumNumberOfRefuelingStops.java
@@ -0,0 +1,49 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+// LC-871
+// https://leetcode.com/problems/minimum-number-of-refueling-stops/
+
+public class MinimumNumberOfRefuelingStops {
+
+  // final stop is the total amount of fuel ever added to the tank
+  public int minRefuelStops(int target, int startFuel, int[][] stations) {
+    int i = 0;
+    int refuel = 0;
+    int stationsSize = stations.length;
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // maxHeap
+
+    for (int maxDistance = startFuel; maxDistance < target; maxDistance += pq.poll()) {
+      while (i < stationsSize && stations[i][0] <= maxDistance) {
+        pq.offer(stations[i++][1]);
+      }
+      if (pq.isEmpty()) {
+        return -1;
+      }
+      refuel++;
+    }
+
+    return refuel;
+  }
+
+  public int minRefuelStopsDP(int target, int startFuel, int[][] stations) {
+    int size = stations.length;
+    long[] dp = new long[size + 1];
+    dp[0] = startFuel;
+    for (int i = 0; i < size; i++) {
+      for (int j = i; j >= 0; j--) {
+        if (dp[j] >= stations[i][0]) {
+          dp[j + 1] = Math.max(dp[j + 1], dp[j] + (long) stations[i][1]);
+        }
+      }
+    }
+    for (int i = 0; i <= size; i++) {
+      if (dp[i] >= target) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
