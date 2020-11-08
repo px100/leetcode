5f89c2ca89e5ed724ae91a16aabc06ca00bfa7eb
ashKIK <ashutheprogrammer@gmail.com>
2020-11-09 00:48:35 +0530

    update LC-445

--- linked-list/AddTwoNumbersII.java
+++ linked-list/AddTwoNumbersII.java
@@ -5,9 +5,97 @@ import java.util.Stack;
 
 public class AddTwoNumbersII {
 
-  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    if (l1 == null || l2 == null) {
-      return java.util.Optional.ofNullable(l1).orElse(l2);
+  public ListNode addTwoNumbersWithoutStacks(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+    if (l2 == null) {
+      return l1;
+    }
+
+    int len1 = 0;
+    int len2 = 0;
+
+    ListNode cur1 = l1;
+    ListNode cur2 = l2;
+
+    while (cur1 != null) {
+      len1++;
+      cur1 = cur1.next;
+    }
+
+    while (cur2 != null) {
+      len2++;
+      cur2 = cur2.next;
+    }
+
+    if (len1 < len2) {
+      ListNode tmp = l1;
+      l1 = l2;
+      l2 = tmp;
+    }
+
+    cur1 = l1;
+    cur2 = l2;
+
+    int offset = Math.abs(len1 - len2);
+    while (offset > 0) {
+      offset--;
+      cur1 = cur1.next;
+    }
+
+    while (cur1 != null) {
+      cur1.val += cur2.val;
+      cur1 = cur1.next;
+      cur2 = cur2.next;
+    }
+
+    cur1 = l1;
+    cur2 = l1.next;
+
+    while (cur2 != null) {
+      while (cur2 != null && cur2.val < 9) {
+        cur1 = cur1.next;
+        cur2 = cur2.next;
+      }
+      if (cur2 == null) {
+        break;
+      }
+      while (cur2 != null && cur2.val == 9) {
+        cur2 = cur2.next;
+      }
+      if (cur2 == null) {
+        break;
+      }
+      if (cur2.val > 9) {
+        cur1.val++;
+        cur1 = cur1.next;
+        while (cur1 != cur2) {
+          cur1.val = 0;
+          cur1 = cur1.next;
+        }
+        cur1.val %= 10;
+      }
+      cur1 = cur2;
+      cur2 = cur1.next;
+    }
+
+    if (l1.val > 9) {
+      ListNode newNode = new ListNode(1);
+      newNode.next = l1;
+      l1.val %= 10;
+      l1 = newNode;
+    }
+
+    return l1;
+  }
+
+  public ListNode addTwoNumbersWithStacks(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+    if (l2 == null) {
+      return l1;
     }
 
     Stack<ListNode> stack1 = new Stack<>();
