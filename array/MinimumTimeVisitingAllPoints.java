0031e35bccadf51e086e77d0ebc3fe0902e7c450
ashKIK <ashutheprogrammer@gmail.com>
2021-04-01 14:13:49 +0530

    update-1266
    Signed-off-by: ashKIK <ashutheprogrammer@gmail.com>

--- array/MinimumTimeVisitingAllPoints.java
+++ array/MinimumTimeVisitingAllPoints.java
@@ -8,7 +8,7 @@ public class MinimumTimeVisitingAllPoints {
     int totalTime = 0;
     for (int i = 0; i < points.length - 1; i++) {
       totalTime += Math.max(
-        Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
+          Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
     }
 
     return totalTime;
