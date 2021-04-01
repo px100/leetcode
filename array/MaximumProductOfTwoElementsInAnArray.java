933c9edd16980b5e2de6b02c56755c6221a216cf
ashKIK <ashutheprogrammer@gmail.com>
2021-04-01 22:19:12 +0530

    add-1464
    Signed-off-by: ashKIK <ashutheprogrammer@gmail.com>

---
+++ array/MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,21 @@
+
+// LC-1464
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+
+public class MaximumProductOfTwoElementsInAnArray {
+
+  // TC: O(n)
+  public int maxProduct(int[] nums) {
+    int max1 = 0;
+    int max2 = 0;
+    for (int num : nums) {
+      if (num > max1) {
+        max2 = max1;
+        max1 = num;
+      } else if (num > max2) {
+        max2 = num;
+      }
+    }
+    return (max1 - 1) * (max2 - 1);
+  }
+}
